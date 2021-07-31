3cdf497dacb8f0055d802d404fac7b9f635b2e57
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-07-31 22:52:04 +0300

    code spacing updated

--- js/scripts.js
+++ js/scripts.js
@@ -56,69 +56,69 @@ Pizza.prototype.calculateTotal = function () {
 // pizza sizes
 const pizzaSizes = [
     {
-      size: "small",
-      price: 600,
+        size: "small",
+        price: 600,
     },
     {
-      size: "medium",
-      price: 800,
+        size: "medium",
+        price: 800,
     },
     {
-      size: "large",
-      price: 1200,
+        size: "large",
+        price: 1200,
     },
-  ];
-  
-  // pizza crusts
-  const pizzaCrusts = [
+];
+
+// pizza crusts
+const pizzaCrusts = [
     {
-      name: "crispy",
-      price: 200,
-    },
-    {
-      name: "stuffed",
-      price: 150,
+        name: "crispy",
+        price: 200,
     },
     {
-      name: "Glutten free",
-      price: 180,
+        name: "stuffed",
+        price: 150,
     },
-  ];
-  
-  //toppings
-  const pizzaToppings = ["Mushrooms", "Pineapple", "Bacon"];
-  
-  const pizzas = [
+    {
+        name: "Glutten free",
+        price: 180,
+    },
+];
+
+//toppings
+const pizzaToppings = ["Mushrooms", "Pineapple", "Bacon"];
+
+const pizzas = [
     { name: "Chicken Tikka" },
     { name: "PeriPeri Pizza" },
     { name: "Raspberry Dessert Pizza" },
     { name: "Chicken Alfredo Pizza" },
     { name: "Sunchoke Pizza" },
     { name: "Buffalo Chicken Sticks" },
-  ];
-  
-  $(function () {
+];
+
+$(function () {
     // append pizzas
     pizzas.forEach((pizza) => {
-      $("#pizza").append(`<option value="${pizza.name}">${pizza.name}</option>`);
+        $("#pizza").append(`<option value="${pizza.name}">${pizza.name}</option>`);
     });
     // append pizza sizes
     pizzaSizes.forEach((pizzaSize) => {
-      $("#size").append(
-        `<option value="${pizzaSize.size}">${pizzaSize.size}-${pizzaSize.price}</option>`
-      );
+        $("#size").append(
+            `<option value="${pizzaSize.size}">${pizzaSize.size}-${pizzaSize.price}</option>`
+        );
     });
-  
+
     // append pizza crusts
     pizzaCrusts.forEach((pizzaCrust) => {
-      $("#crust").append(
-        `<option value="${pizzaCrust.name}">${pizzaCrust.name}-${pizzaCrust.price}</option>`
-      );
+        $("#crust").append(
+            `<option value="${pizzaCrust.name}">${pizzaCrust.name}-${pizzaCrust.price}</option>`
+        );
     });
-  
+
     //append pizza toppings
     pizzaToppings.forEach((topping) => {
-      $(".toppings").append(`<div class="col-md-6">
+        $(".toppings").append(`<div class="col-md-6">
         <div class="form-check">
           <input class="form-check-input" name="toppings[]" type="checkbox" id="${topping}" value="${topping}">
           <label class="form-check-label" for="${topping}">
@@ -127,97 +127,95 @@ const pizzaSizes = [
           </div>
         </div>`);
     });
-  
+
     // function to calculate grand total
     function calculateGrandTotal() {
         let total = 0;
         cart.forEach((pizza) => {
-          total += pizza.price;
+            total += pizza.price;
         });
-    
+
         $(".grand-total").text(total);
-      }
-    
-      // initialize an empty cart
-      const cart = [];
-    
-      $("#order-form").on("submit", function (e) {
+    }
+
+    // initialize an empty cart
+    const cart = [];
+
+    $("#order-form").on("submit", function (e) {
         //prevent default action
         e.preventDefault();
-    
+
         const selectedPizzaName = $("#pizza").val();
         const selectedSize = $("#size").val();
         const selectedCrust = $("#crust").val();
         const selectedToppings = $("input[name='toppings[]']:checkbox:checked")
-          .map(function () {
-            return $(this).val();
-          })
-          .get();
-    
+            .map(function () {
+                return $(this).val();
+            })
+            .get();
+
         // put validation for all fields
-    
+
         // cart details
         //check if selected pizza exists in cart
         const cartPizza = cart.find((pizza) => {
-          const sameToppings =
-            JSON.stringify(pizza.toppings) == JSON.stringify(selectedToppings);
-    
-          return (
-            pizza.name == selectedPizzaName &&
-            pizza.size.size == selectedSize &&
-            sameToppings
-          );
+            const sameToppings =
+                JSON.stringify(pizza.toppings) == JSON.stringify(selectedToppings);
+
+            return (
+                pizza.name == selectedPizzaName &&
+                pizza.size.size == selectedSize &&
+                sameToppings
+            );
         });
         //if it exists increase quantity
         if (cartPizza) {
-          cartPizza.setQuantity(cartPizza.quantity + 1);
+            cartPizza.setQuantity(cartPizza.quantity + 1);
         } else {
-          const pizza = new Pizza(selectedPizzaName);
-          pizza.setSize(selectedSize);
-          pizza.setCrust(selectedCrust); 
-          pizza.setTopings(selectedToppings);
-    
-          cart.push(pizza);
+            const pizza = new Pizza(selectedPizzaName);
+            pizza.setSize(selectedSize);
+            pizza.setCrust(selectedCrust);
+            pizza.setTopings(selectedToppings);
+
+            cart.push(pizza);
         }
         // empty tbody first
         $(".order-table tbody").html("");
         //loop and append
         cart.forEach((pizza, cartIndex) => {
-          $(".order-table tbody").append(`
+            $(".order-table tbody").append(`
             <tr>
                 <td>${pizza.name}</td>
                 <td>${pizza.size.size}</td>
                 <td>${pizza.crust.name}</td>
                 <td>${pizza.toppings.join(", ")}</td>
                 <td>
-                    <input type="number" min="1" class="input-sm form-control pizza-quantity" data-cart-index="${cartIndex}" value="${
-            pizza.quantity
-          }" />
+                    <input type="number" min="1" class="input-sm form-control pizza-quantity" data-cart-index="${cartIndex}" value="${pizza.quantity
+                }" />
                 </td>
                 <td>${pizza.price}</td>
             </tr>
         `);
-    
-          //update grand total
-          calculateGrandTotal();
+
+            //update grand total
+            calculateGrandTotal();
         });
-      });
-     //pizza quantity change event
-     $("body").on("change", ".pizza-quantity", function () {
+    });
+    //pizza quantity change event
+    $("body").on("change", ".pizza-quantity", function () {
         const quantity = $(this).val();
         const cartIndex = $(this).data("cart-index");
         const pizza = cart[cartIndex];
-    
+
         if (quantity > 0) {
-          pizza.setQuantity(quantity);
-          // update line total
-          $(this).parent().next().text(pizza.price);
+            pizza.setQuantity(quantity);
+            // update line total
+            $(this).parent().next().text(pizza.price);
         }
-    
+
         //update grand total
         calculateGrandTotal();
-      });
-     
-    
-  });
-  
+    });
+
+
+});
