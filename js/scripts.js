287cff46a60055d1864a5db06d2516ebe945d381
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-07-31 22:46:32 +0300

    code refactor done

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,54 @@
 // pizza constructor
 function Pizza(name) {
-  this.name = name;
-  this.price = 0;
-  this.quantity = 1;
-  this.toppings = [];
+    this.name = name;
+    this.price = 0;
+    this.quantity = 1;
+    this.toppings = [];
 }
 
 // set pizza size
 Pizza.prototype.setSize = function (size) {
-  const pizzaSize = pizzaSizes.find((pizzaSize) => pizzaSize.size == size);
-  if (pizzaSize) {
-    this.size = pizzaSize;
-    this.calculateTotal();
-  }
+    const pizzaSize = pizzaSizes.find((pizzaSize) => pizzaSize.size == size);
+    if (pizzaSize) {
+        this.size = pizzaSize;
+        this.calculateTotal();
+    }
 };
 
 //set pizza crust
 Pizza.prototype.setCrust = function (name) {
-  const pizzaCrust = pizzaCrusts.find((pizzaCrust) => pizzaCrust.name == name);
-  if (pizzaCrust) {
-    this.crust = pizzaCrust;
-    this.calculateTotal();
-  }
+    const pizzaCrust = pizzaCrusts.find((pizzaCrust) => pizzaCrust.name == name);
+    if (pizzaCrust) {
+        this.crust = pizzaCrust;
+        this.calculateTotal();
+    }
 };
 
 Pizza.prototype.setTopings = function (toppings) {
-  this.toppings = toppings;
-  this.calculateTotal();
+    this.toppings = toppings;
+    this.calculateTotal();
 };
 
 //set quantity
 Pizza.prototype.setQuantity = function (quantity) {
-  this.quantity = +quantity;
-  this.calculateTotal();
+    this.quantity = +quantity;
+    this.calculateTotal();
 };
 
 // calculate pizza total
 Pizza.prototype.calculateTotal = function () {
-  const toppingPrice = 50;
+    const toppingPrice = 50;
 
-  if (this.size) {
-    this.price = this.size.price;
-  }
+    if (this.size) {
+        this.price = this.size.price;
+    }
 
-  if (this.crust) {
-    this.price = this.price + this.crust.price;
-  }
+    if (this.crust) {
+        this.price = this.price + this.crust.price;
+    }
 
-  // add the price of toppings
-  this.price += this.toppings.length * toppingPrice;
+    // add the price of toppings
+    this.price += this.toppings.length * toppingPrice;
 
-  this.price *= this.quantity;
+    this.price *= this.quantity;
 };
